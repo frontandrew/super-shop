d6ac1ca8b388b8b06f403193374dafa64049048c
Andrew Bubnov <jackblack.nt@ya.ru>
2020-07-02 16:08:16 +0500

    Some semantic has been added to Content component.

--- src/scirpts/components/content/content.js
+++ src/scirpts/components/content/content.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import './content.css';
+
 import CategoriesList from '../categories-list/categories-list.js';
 import ItemsList from '../items-list/items-list.js';
 import Product from '../product/product.js';
 
 const Content = ({ view }) => {
-  if (view === 'categories') {
-    return (
-      <CategoriesList />
-    )
-  } else if (view === 'items') {
-    return (
-      <ItemsList />
-    )
-  } else if (view === 'product') {
-    return (
-      <Product />
-    )
+
+  const content = () => {
+    if (view === 'categories') {
+      return (
+        <CategoriesList />
+      )
+    } else if (view === 'items') {
+      return (
+        <ItemsList />
+      )
+    } else if (view === 'product') {
+      return (
+        <Product />
+      )
+    }
   }
+
+  return (
+    <main className="content">
+      {content()}
+    </main>
+  )
 }
 
 const mapStateToProps = (state) => {
