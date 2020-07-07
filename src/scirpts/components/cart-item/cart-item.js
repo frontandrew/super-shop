522216b9b6d128de0d1b62b4ad91b583a475f9f2
Andrew Bubnov <jackblack.nt@ya.ru>
2020-07-07 15:59:32 +0500

    Cart-item component has been implemented with react hoocks. Now input element is controlled.

--- src/scirpts/components/cart-item/cart-item.js
+++ src/scirpts/components/cart-item/cart-item.js
@@ -1,65 +1,43 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 import './cart-item.css';
 
-class CartItem extends Component {
-  state = {
-    input: {},
-  }
+const CartItem = ({ item, onAdd, onRemove, onDelete }) => {
 
-  onInputChange(event) {
-    const diff = event.target.value - this.state.input;
+  const { title, quantity, inCart } = item;
+
+  const [value, setValue] = useState({})
+
+  useEffect(() => setValue(inCart));
+
+  const onInputChange = (event) => {
+    const diff = event.target.value - value;
 
     if (diff > 0) {
-      this.props.onAdd(this.props.item, diff)
-      this.setState({
-        input: event.target.value,
-      })
+      onAdd(item, diff)
     } else {
-      this.props.onRemove(this.props.item, diff)
-      this.setState({
-        input: event.target.value,
-      })
+      onRemove(item, diff)
     }
   }
 
-  componentDidMount() {
-    this.setState({
-      input: this.props.item.inCart,
-    })
-  }
+  return (
+    <Fragment>
+      <h4 className="cart-item__name">{title}</h4>
+      <input
+        className="cart-item__quantity"
+        onChange={(event) => onInputChange(event)}
+        type="number"
+        min={1}
+        max={quantity}
+        value={value} />
+      <button
+        className="cart-item__delete"
+        onClick={() => onDelete(item, -inCart)}>
+        X
+      </button>
+    </Fragment>
+  )
 
-  /*   componentDidUpdate(prevProps, prevState) {
-      if ( !== this.state.input) {
-        const diff = this.state.input - prevState.input;
-  
-        if (diff > 0) {
-          this.props.onAdd(this.props.item, diff)
-        }
-      }
-    } */
-
-  render() {
-    const { title, quantity, inCart } = this.props.item;
-
-    return (
-      <Fragment>
-        <h4 className="cart-item__name">{title}</h4>
-        <input
-          className="cart-item__quantity"
-          onChange={(event) => this.onInputChange(event)}
-          type="number"
-          min={1}
-          max={quantity}
-          value={this.state.input} />
-        <button
-          className="cart-item__delete" 
-          onClick={() => this.props.onDelete(this.props.item, -inCart)}>
-          X
-        </button>
-      </Fragment>
-    )
-  }
 }
 
 export default CartItem;
